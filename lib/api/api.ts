624d3068fb2aff2123504dd003d9adc08b86cda3
Raffaello-creator <wolf201026@gmail.com>
2025-07-16 09:09:34 +0200

    fix bug

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -45,5 +45,5 @@ import axios from "axios";
 
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
-  withCredentials: true,
+  withCredentials: false,
 });
