c05cfda7c6599e87bf5a00b23467acfb14a175e4
Raffaello-creator <wolf201026@gmail.com>
2025-07-17 08:45:15 +0200

    fix bug

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,32 +1,51 @@
 import { NextRequest, NextResponse } from "next/server";
-import { api } from "@/app/api/api";
+import { api } from "@/lib/api/api";
 import { cookies } from "next/headers";
 import { parse } from "cookie";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const apiRes = await api.post("auth/register", body);
+  try {
+    const body = await req.json();
+    const apiRes = await api.post("auth/register", body);
 
-  const cookieStore = await cookies();
-  const setCookie = apiRes.headers["set-cookie"];
+    const cookieStore = await cookies();
+    const setCookie = apiRes.headers["set-cookie"];
 
-  if (setCookie) {
-    const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-    for (const cookieStr of cookieArray) {
-      const parsed = parse(cookieStr);
+    if (setCookie) {
+      const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+      for (const cookieStr of cookieArray) {
+        const parsed = parse(cookieStr);
 
-      const options = {
-        expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-        path: parsed.Path,
-        maxAge: Number(parsed["Max-Age"]),
-      };
-      if (parsed.accessToken)
-        cookieStore.set("accessToken", parsed.accessToken, options);
-      if (parsed.refreshToken)
-        cookieStore.set("refreshToken", parsed.refreshToken, options);
+        const options = {
+          expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+          path: parsed.Path,
+          maxAge: Number(parsed["Max-Age"]),
+        };
+        if (parsed.accessToken)
+          cookieStore.set("accessToken", parsed.accessToken, options);
+        if (parsed.refreshToken)
+          cookieStore.set("refreshToken", parsed.refreshToken, options);
+      }
+      return NextResponse.json(apiRes.data);
     }
-    return NextResponse.json(apiRes.data);
-  }
 
-  return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  } catch (error: unknown) {
+    let errorMessage = "Server error";
+
+    type ApiError = {
+      response?: { data?: string };
+      message?: string;
+    };
+
+    if (typeof error === "object" && error !== null) {
+      const err = error as ApiError;
+      if ("response" in err && typeof err.response?.data === "string") {
+        errorMessage = err.response.data;
+      } else if ("message" in err && typeof err.message === "string") {
+        errorMessage = err.message;
+      }
+    }
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
 }
