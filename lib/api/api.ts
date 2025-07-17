d08c91f6199b6c98b4f9f07392b0a5d48d496870
Raffaello-creator <wolf201026@gmail.com>
2025-07-17 08:37:58 +0200

    fix bug

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,49 +1,18 @@
 // import axios from "axios";
-// import type { Note, NewNote } from "../../types/note";
-// import { ResponseGetData } from "@/types/ResponseGetData";
 
-// axios.defaults.baseURL = "https://notehub-public.goit.study/api";
-// const notehubToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+// export const nextServer = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
 
-// const headers = {
-//   Accept: "application/json",
-//   Authorization: `Bearer ${notehubToken}`,
-// };
-
-// export async function fetchNotes(
-//   page: number,
-//   searchText: string,
-//   tag?: string
-// ): Promise<ResponseGetData> {
-//   const { data } = await axios.get<ResponseGetData>("/notes", {
-//     params: {
-//       page,
-//       perPage: 16,
-//       ...(searchText !== "" ? { search: searchText } : {}),
-//       ...(tag !== "All" ? { tag } : {}),
-//     },
-//     headers,
-//   });
-//   return data;
-// }
-
-// export async function createNote(newNote: NewNote): Promise<Note> {
-//   const { data } = await axios.post<Note>("/notes", newNote, { headers });
-//   return data;
-// }
-
-// export async function deleteNote(noteId: number): Promise<Note> {
-//   const { data } = await axios.delete<Note>(`/notes/${noteId}`, { headers });
-//   return data;
-// }
-
-// export async function fetchNoteById(noteId: number): Promise<Note> {
-//   const { data } = await axios.get<Note>(`/notes/${noteId}`, { headers });
-//   return data;
-// }
+//   withCredentials: true,
+// });
 import axios from "axios";
 
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
-  withCredentials: false,
+  baseURL: "/api",
+  withCredentials: true,
+});
+
+export const api = axios.create({
+  baseURL: "https://notehub-api.goit.study",
+  withCredentials: true,
 });
